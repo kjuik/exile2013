aa38cc04684d10bc9d72b61482efe9995e3eb44e
Horatiu Roman <croman12@student.aau.dk>
2013-11-01 19:33:54 +0100

    rotation info of the body

---
+++ Assets/Scripts/DogScripts/RotationInfo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationInfo : MonoBehaviour {
+	
+	public Quaternion initRotation;
+	public float rotationZDelta;
+	
+	void Start() {
+		initRotation = transform.rotation;
+		
+	}
+	
+	void Update() {
+		
+		float initR = initRotation.eulerAngles.z;
+		if (initR > 180)
+			initR = initR - 360;
+		float curR = transform.rotation.eulerAngles.z;
+		if (curR > 180) 
+			curR = curR - 360;
+		// the difference of balance
+		rotationZDelta = curR - initR;
+		
+	}
+	
+	
+}
